c9c783b95c0fe9f26c897cdd68fd40f81feec228
supraaxdd <supraaxdd@github.com>
2025-06-16 16:57:49 +0100

    Included some TODO comments for future development in trainer.py

--- ai/core/trainer.py
+++ ai/core/trainer.py
@@ -1,2 +1,16 @@
 def train():
 	print("Training model...")
+
+	 # TODO: Import and prepare your training data
+    # from data_loader import load_training_data
+    # X_train, y_train = load_training_data()
+
+    # TODO: Build and compile model
+    # from model_builder import build_lstm_model
+    # model = build_lstm_model(input_shape=X_train.shape[1:], learning_rate=lr)
+
+    # TODO: Train the model
+    # model.fit(X_train, y_train, epochs=epochs, batch_size=32)
+
+    # TODO: Save the model
+    # model.save("saved/model.keras")
